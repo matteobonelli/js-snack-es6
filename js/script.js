aa56f5fb6c63d86232743c9389883784f07f298b
matteobonelli <matteobonelli08@gmail.com>
2023-10-24 16:18:40 +0200

    created an uppercase list of names

--- js/script.js
+++ js/script.js
@@ -11,3 +11,45 @@ const seatMarker = vipList.map((element, index) => {
 })
 
 console.log(seatMarker)
+
+const students = [
+    {
+        id : 213,
+        name : 'Marco della Rovere',
+        grades : 78
+    },
+    {
+        id : 110,
+        name : 'Paola Cortellessa',
+        grades : 96
+    },
+    {
+        id : 250,
+        name : 'Andrea Mantegna',
+        grades : 48
+    },
+    {
+        id : 145,
+        name : 'Gaia Borromini',
+        grades : 74
+    },
+    {
+        id : 196,
+        name : 'Luigi Grimaldello',
+        grades : 68
+    },
+    {
+        id : 102,
+        name : 'Piero della Francesca',
+        grades : 50
+    },
+    {
+        id : 120,
+        name : 'Francesca da Polenta',
+        grades : 84
+    },
+]
+
+ let nameStudentsList = students.map((element) => element.name.toUpperCase())
+
+ console.log(nameStudentsList);
